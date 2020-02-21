e6b16d079ff2b7ae6a4089a8629135c3fea255cd
sofiafarina <sofia.farina2@studio.unibo.it>
2020-02-21 16:04:26 +0100

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -4,8 +4,10 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import networkx as nx
 
+# Periodi: (A) da 12, (B) da 112, (C) da 212, (D) da 312, (E) da 400
 
-adjacency = np.load("adj_12.npy")
+
+adjacency = np.load("Desktop/adj_12.npy")
 
 print("creating graph")
 G = nx.from_numpy_matrix(adjacency, parallel_edges=False)
@@ -18,13 +20,18 @@ G.remove_edges_from((e for e, w in edge_weights.items() if w > 20000.))
 dopo = G.number_of_edges()
 
 #%%
+# selection of connected components 
+S = [G.subgraph(c).copy() for c in nx.connected_components(G)]
+largest_connected = S[1] # assumo siano ordinati, è giusto?
+
+#%%
 
 print("computing degree histogram")
 deg_histo = nx.degree_histogram(G)
 # Returns a list of the frequency of each degree value.
 
 plt.hist(deg_histo, bins =100, range = (0,50))
-plt.show()
+plt.title("degree histogram (E)")
 
 #%%
 
@@ -35,21 +42,22 @@ print("density is", density)
 #%%
 
 print("computing diameter")
-diameter = nx.diameter(G)
+diameter = nx.diameter(largest_connected)
 print("diameter is", diameter)
-# NetworkXError: Found infinite path length because the graph is not connected
+#long long 
 
 #%%
 print("computing the average clustering")
 avg_clust = nx.average_clustering(G)
 print("average clustering is", avg_clust)
-# lungo
+# un poco lungo (dalle 11.32 alle 11.38)
 
 #%%
 
 print("computing laplacian spectrum")
 lap_spec = nx.laplacian_spectrum(G, weight = 'weight')
 plt.plot(lap_spec)
+plt.title("laplacian spectrum (E)")
 # prendere i primi autovalori e vedere la loro molteplicità
 # non è troppo lento nel calcolo
 
@@ -58,13 +66,14 @@ plt.plot(lap_spec)
 print("computing centrality measures")
 deg_cent = nx.degree_centrality(G)
 plt.hist(pd.Series(deg_cent), bins = 50)
-plt.title("degree centrality")
+plt.title("degree centrality (E)")
 # non è lento
 
 #%%
 
 print("computing betwenness measures")
 bet_cent = nx.betweenness_centrality(G)
+# molto lungo
 
 #%%
 
@@ -76,3 +85,16 @@ clos_cent = nx.closeness_centrality(G)
 print("computing avg shst path length")
 avg_s_path = nx.average_shortest_path_length(G)
 
+#%%
+r = ['A','B','C','D','E']
+dst = [0.03070465907372277,0.0272638272617172, 0.036226356899325615, 0.03430904850624072, 0.03699926434424521]
+plt.ylim(0.01,0.05)
+plt.plot(r,dst)
+plt.title("Density variation")
+
+#%%
+r = ['A','B','C','D','E']
+avgcl = [ 0.3987558317924219 ,0.3827655402340547, 0.4163728385629933,  0.41910714676578453, 0.41157384408015824]
+plt.ylim(0.3,0.5)
+plt.plot(r,avgcl)
+plt.title("Average clustering variation")
